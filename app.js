4239104418e05d3a1999ddb38dda084c6b898600
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-21 12:47:42 -0700

    displayCountries!!

--- app.js
+++ app.js
@@ -64,6 +64,8 @@ function displayCountries() {
 
     for (const country of countries) {
         // > Part A: render and append to list
+        const countryEl = renderCountry(country);
+        countryList.append(countryEl);
     }
 }
 
